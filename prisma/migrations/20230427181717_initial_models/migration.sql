172913918c213ed1bf20bed7833d66cf42a0eb3a
Manoel Duran <manoel.duran@hotmail.com>
2023-04-27 16:29:35 -0300

    feat(migrations): created prisma migrations

---
+++ prisma/migrations/20230427181717_initial_models/migration.sql
@@ -0,0 +1,58 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "chats" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "chats_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" TEXT NOT NULL,
+    "chatId" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "sentAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ChatUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ChatUser_AB_unique" ON "_ChatUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ChatUser_B_index" ON "_ChatUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "chats"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ChatUser" ADD CONSTRAINT "_ChatUser_A_fkey" FOREIGN KEY ("A") REFERENCES "chats"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ChatUser" ADD CONSTRAINT "_ChatUser_B_fkey" FOREIGN KEY ("B") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/shared/infra/orm/prisma.ts
+++ src/shared/infra/orm/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-
-export { prisma }
+async function getMessagesByUser(userId: string) {
+    const messages = await prisma.message.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        chat: true,
+      },
+    })
+    return messages
+  }
+export { prisma, getMessagesByUser }
