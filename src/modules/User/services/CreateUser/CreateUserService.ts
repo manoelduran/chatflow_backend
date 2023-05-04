37b8da2745b9e16077a369012b55f9b4ae35ee84
Manoel Duran <manoel.duran@hotmail.com>
2023-05-04 13:22:30 -0300

    refact(CreateUser): added hash to password

--- src/modules/User/services/CreateUser/CreateUserService.ts
+++ src/modules/User/services/CreateUser/CreateUserService.ts
@@ -2,6 +2,7 @@ import { CreateUserDTO } from "@modules/User/dtos/CreateUserDTO";
 import { UserAlreadyExistsException } from "@modules/User/exceptions/UserAlreadyExistsException";
 import { IUsersRepository } from "@modules/User/repositories/IUsersRepository";
 import { CreateUserResponse } from "@modules/User/responses/CreateUserResponse";
+import { IHashProvider } from "@shared/container/providers/HashProvider/models/IHashProvider";
 import { left, right } from "@shared/either";
 import { inject, injectable } from "tsyringe";
 
@@ -10,16 +11,18 @@ import { inject, injectable } from "tsyringe";
 class CreateUserService {
     constructor(
         @inject("UsersRepository")
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+        @inject("HashProvider")
+        private hashProvider: IHashProvider
     ) { }
     async execute(data: CreateUserDTO): CreateUserResponse {
-    
         const userAlreadyExists = await this.usersRepository.findByWhere(data.username, data.email);
         if (userAlreadyExists.isRight()) {
             return left(new UserAlreadyExistsException());
         };
         console.log('data', data)
-        const newUser = await this.usersRepository.create(data);
+        const hashedPassword = await this.hashProvider.generateHash(data.password);
+        const newUser = await this.usersRepository.create({ ...data, password: hashedPassword });
 
         return right(newUser);
     };
