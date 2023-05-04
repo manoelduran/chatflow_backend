01308de47ba2b1f360f00052a557bf81ed9643b6
Manoel Duran <manoel.duran@hotmail.com>
2023-05-04 13:25:27 -0300

    feat(userRoutes): added auth route

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,9 +1,11 @@
+import { AuthController } from "@modules/User/infra/http/controllers/AuthController";
 import { UserController } from "@modules/User/infra/http/controllers/UserController";
 import { celebrate, Joi, Segments } from "celebrate";
 import { Router } from "express";
 
 
 const userRoutes = Router();
+const authController = new AuthController()
 
 const userController = new UserController();
 
@@ -15,6 +17,15 @@ userRoutes.post("/", celebrate({
     },
   }),
   userController.create
+  );
+
+  userRoutes.post("/auth", celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().required(),
+      password: Joi.string().required()
+    },
+  }),
+  authController.auth
   );
 
 userRoutes.get("/", userController.list)
