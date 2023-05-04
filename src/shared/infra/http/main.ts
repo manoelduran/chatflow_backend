b175ef229b2b6dd67e76ae244348c44e304741a9
Manoel Duran <manoel.duran@hotmail.com>
2023-05-04 13:24:28 -0300

    refact(main): added white list to cors and some data to request type

--- src/shared/infra/http/main.ts
+++ src/shared/infra/http/main.ts
@@ -11,7 +11,14 @@ import { errors } from 'celebrate';
 import { httpExceptionHandler } from './middlewares/httpExceptionHandler';
 import { listen } from '../../../socket';
 
+interface ConnectedUsers {
+    [key: string]: string;
+}
 class Main {
+    private connectedUsers: ConnectedUsers = {};
+    private allowedOrigins: string[] = [
+        'http://localhost:3000',
+    ];
     public app: Application;
     public server: HttpServer
     public io: socketio.Server
@@ -36,14 +43,34 @@ class Main {
         this.exceptionHandler()
     }
     private middlewares() {
-        this.app.use(cors());
         this.app.use(express.json());
+        this.app.use((req, res, next) => {
+            req.io = this.io;
+            req.connectedUsers = this.connectedUsers;
+            return next();
+        });
 
+        this.app.use(
+            cors({
+                origin: (origin, callback) => {
+                    if (!origin) return callback(null, true);
+
+                    if (this.allowedOrigins.indexOf(origin) === -1) {
+                        const message = `A política CORS para este site não permite acesso da Origem ${origin}.`;
+
+                        return callback(new Error(message), false);
+                    }
+
+                    return callback(null, true);
+                },
+                credentials: true,
+            }),
+        );
     }
     private exceptionHandler(): void {
         this.app.use(errors());
         this.app.use(httpExceptionHandler.handle);
-      }
+    }
     private socketIo(): void {
         listen(this.io)
     }
