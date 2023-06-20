3618d54f02cf9a9768eaebf4be628d525e53c8ff
Manoel Cendon Duran <manoelcendonduran@Manoels-MacBook-Air.local>
2023-06-20 02:01:10 -0300

    feat(ListmessagesByChat): created service to list messages by chat

--- src/modules/Message/services/ListMessagesByChat/ListMessagesByChatService.ts
+++ src/modules/Message/services/ListMessagesByChat/ListMessagesByChatService.ts
@@ -2,7 +2,7 @@ import { IChatsRepository } from "@modules/Chat/repositories/IChatsRepository";
 import { ListMessagesByChatDTO } from "@modules/Message/dtos/ListMessagesByChatDTO";
 import { IMessagesRepository } from "@modules/Message/repositories/IMessagesRepository";
 import { ListMessagesByChatResponse } from "@modules/Message/responses/ListMessagesByChatResponse";
-import { left } from "@shared/either";
+import { left, right } from "@shared/either";
 import { inject, injectable } from "tsyringe";
 
 
@@ -20,8 +20,11 @@ class ListMessagesByChatService {
             return left(chatOrError.value);
         };
         const messages = await this.messagesRepository.findAllByChat(chatOrError.value.id);
-        console.log('messages', messages)
-        return messages;
+        if(messages.isLeft()) {
+            return left(messages.value)
+        }
+        console.log('messages', messages.value)
+        return right(messages.value);
     };
 };
 
