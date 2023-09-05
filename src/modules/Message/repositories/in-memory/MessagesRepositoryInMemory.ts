0d0c1a72a515b6d2dd489777f096a03c60a8a727
Manoel Duran <manoel.duran@hotmail.com>
2023-09-04 23:34:38 -0300

    feat(MessagesRepositoryInMemory): added in memory messages repository

--- src/modules/Message/repositories/in-memory/MessagesRepositoryInMemory.ts
+++ src/modules/Message/repositories/in-memory/MessagesRepositoryInMemory.ts
@@ -1,4 +1,4 @@
-
+import { v4 as uuidV4 } from 'uuid';
 import { Either, left, right } from '@shared/either';
 import { IMessagesRepository } from '@modules/Message/repositories/IMessagesRepository';
 import { MessageModel } from '@modules/Message/infra/persistence/models/Message';
@@ -13,17 +13,37 @@ class MessagesRepositoryInMemory implements IMessagesRepository {
     constructor() {
         this.messages = [];
     }
-    findSpecificMessage(chat_id: string, user_id: string, text: string): Promise<Either<MessageNotFoundException, MessageModel>> {
-        throw new Error('Method not implemented.');
+    async findAllUserMessagesInChat(chat_id: string, user_id: string): Promise<Either<MessageNotFoundException, MessageModel[]>> {
+        const messages = this.messages.filter(message => message.chatId === chat_id && message.userId === user_id);
+        if (!messages) {
+            return left(new MessageNotFoundException());
+        };
+        return right(messages);
     }
-    findAllUserMessagesInChat(chat_id: string, user_id: string): Promise<Either<MessageNotFoundException, MessageModel>> {
-        throw new Error('Method not implemented.');
+    async findAllByChat(chat_id: string): Promise<Either<MessageNotFoundException, MessageModel[]>> {
+        const messages = this.messages.filter(message => message.chatId === chat_id);
+        if (!messages) {
+            return left(new MessageNotFoundException());
+        };
+        return right(messages);
     }
-    findAllByUser(user_id: string): Promise<Either<MessageNotFoundException, MessageModel[]>> {
-        throw new Error('Method not implemented.');
+    async findSpecificMessage(chat_id: string, user_id: string, text: string): Promise<Either<MessageNotFoundException, MessageModel>> {
+        const messageOrError = this.messages.find(message => message.chatId === chat_id && message.userId === user_id && message.text);
+        if (!messageOrError) {
+            return left(new MessageNotFoundException())
+        }
+        return right(messageOrError);
+    }
+
+    async findAllByUser(user_id: string): Promise<Either<MessageNotFoundException, MessageModel[]>> {
+        const messages = this.messages.filter(message => message.userId === user_id);
+        if (!messages) {
+            return left(new MessageNotFoundException());
+        };
+        return right(messages);
     }
     async findByName(name: string): Promise<Either<MessageNotFoundException, MessageModel>> {
-        const message = this.messages.find(message => message.name === name);
+        const message = this.messages.find(message => message.text === name);
         if (!message) {
             return left(new MessageNotFoundException());
         };
@@ -37,7 +57,13 @@ class MessagesRepositoryInMemory implements IMessagesRepository {
         return right(message);
     };
     async create(data: CreateMessageDTO): Promise<MessageModel> {
-        const newMessage = new MessageModel();
+        const newMessage = new MessageModel({
+            ...data,
+            id: uuidV4(),
+            chatId: data.chat_id,
+            sentAt: new Date(),
+            userId: data.user_id
+        });
         Object.assign(newMessage, data);
         this.messages.push(newMessage);
         return newMessage;
