492f1d84fe02e9ae24ae664de060cec9603fef0a
Manoel Duran <manoel.duran@hotmail.com>
2023-09-04 23:33:32 -0300

    fix(CreateChatDto): added values to fake repository

--- src/modules/Chat/dtos/CreateChatDTO.ts
+++ src/modules/Chat/dtos/CreateChatDTO.ts
@@ -3,6 +3,8 @@ import { UserModel } from "@modules/User/infra/persistence/models/User";
 interface CreateChatDTO {
     name: string;
     user?: UserModel;
+    owner_id?: string;
+    created_at?: string;
     user_id?: string;
 }
 
