ceba421772b894bd764e2547daf715cd9e66acd5
Manoel Cendon Duran <manoel.duran@hotmail.com>
2023-10-09 21:29:09 -0300

    test workflow

--- src/socket.ts
+++ src/socket.ts
@@ -1,31 +1,33 @@
-import socketio from 'socket.io';
+import socketio from "socket.io";
 
 export const listen = (io: socketio.Server) => {
-
-    io.on('connection', (socket) => {
-        socket.emit("hello", "world");
-        socket.on("pao", (arg) => {
-            console.log(arg)
-        })
-        socket.on("create-room", (arg) => {
-            console.log("args create", arg)
-            socket.broadcast.emit("created",` ${arg.user.user.username} created a new chat! Let's join in it!`)
-        })
-        socket.on("join-room", (arg) => {
-            console.log("args join", arg)
-            socket.broadcast.emit("joined",` ${arg} joined to our chat!`)
-        })
-        socket.on("authenticate", (arg) => {
-            console.log("arg auth", arg)
-            socket.broadcast.emit("authenticated",` ${arg} has been authenticated!`)
-        })
-        socket.on("message", (arg) => {
-            console.log("arg msg", arg)
-            socket.broadcast.emit("sent",` ${arg.text} has been sent!`)
-        })
+  io.on("connection", (socket) => {
+    socket.emit("hello", "world");
+    socket.on("pao", (arg) => {
+      console.log(arg);
     });
-}
+    socket.on("create-room", (arg) => {
+      console.log("args create", arg);
+      socket.broadcast.emit(
+        "created",
+        ` ${arg.user.user.username} created a new chat! Let's join in it!`
+      );
+    });
+    socket.on("join-room", (arg) => {
+      console.log("args join", arg);
+      socket.broadcast.emit("joined", ` ${arg} joined to our chat!`);
+    });
+    socket.on("authenticate", (arg) => {
+      console.log("arg auth", arg);
+      socket.broadcast.emit("authenticated", ` ${arg} has been authenticated!`);
+    });
+    socket.on("message", (arg) => {
+      console.log("arg msg", arg);
+      socket.broadcast.emit("sent", ` ${arg.text} has been sent!`);
+    });
+  });
+};
 
 module.exports = {
-    listen
-}
+  listen,
+};
