41d21c0190045810dcb9497e17d0745cc342f45a
Manoel Cendon Duran <manoel.duran@hotmail.com>
2023-10-14 17:30:18 -0300

    test workflow

--- src/modules/Message/services/ListMessages/ListMessagesService.ts
+++ src/modules/Message/services/ListMessages/ListMessagesService.ts
@@ -2,17 +2,16 @@ import { IMessagesRepository } from "@modules/Message/repositories/IMessagesRepo
 import { ListMessagesResponse } from "@modules/Message/responses/ListMessagesResponse";
 import { inject, injectable } from "tsyringe";
 
-
 @injectable()
 class ListMessagesService {
-    constructor(
-        @inject("MessagesRepository")
-        private messagesRepository: IMessagesRepository
-    ) { }
-    async execute(): ListMessagesResponse {
-        const messages = await this.messagesRepository.list();
-        return messages;
-    };
-};
+  constructor(
+    @inject("MessagesRepository")
+    private messagesRepository: IMessagesRepository
+  ) {}
+  async execute(): ListMessagesResponse {
+    const messages = await this.messagesRepository.list();
+    return messages;
+  }
+}
 
 export { ListMessagesService };
